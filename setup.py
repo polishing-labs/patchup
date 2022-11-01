11c59e27743cb6574a4eff1e920d23d103c7c96e
ttimochan <teemo@timochan.cn>
2022-11-01 22:18:56 +0800

    chore: update

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 # coding:utf-8
+'''
+  @Author: ZIKH26
+  @Date: 2022-03-01 20:59:51
+LastEditors: ttimochan
+LastEditTime: 2022-11-01 22:18:44
+FilePath: /patchup/setup.py
+'''
 from setuptools import setup
 
 setup(
